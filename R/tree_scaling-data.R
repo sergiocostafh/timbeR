be7e54c1b8564d734381ccb272b69075758e505a
sergiocostafh <sergio.vscf@gmail.com>
2021-08-20 09:00:54 -0300

    Update  2021-08-20

---
+++ R/tree_scaling-data.R
@@ -0,0 +1,8 @@
+#' Tree scaling example data
+#'
+#' Diameter (cm) and height (m) measurements along the bole of 8 Pinus taeda trees.
+#'
+#' @docType data
+#'
+#' @usage data(tree_scaling)
+"tree_scaling"
