a8bc241092556cad7fc6a75c7fd38480c901efa1
sergiocostafh <sergio.vscf@gmail.com>
2021-11-29 21:26:11 -0300

    Update  2021-11-29

--- R/kozak_hi.R
+++ R/kozak_hi.R
@@ -18,7 +18,7 @@
 kozak_hi <- function(dbh, h, di, coef, p){
   b0 <- coef[1]; b1 <- coef[2]; b2 <- coef[3]; b3 <- coef[4]; b4 <- coef[5]; b5 <- coef[6]; b6 <- coef[7]; b7 <- coef[8]; b8 <- coef[9]
   fun_opt_hi <- function(hi){
-    (  b0*(dbh^b1)*(h^b2)*((1-(hi/h)^(1/4))/(1-(p^(1/3))))^(b3*(hi/h)^4+b4*(1/exp(dbh/h))+b5*((1-(hi/h)^(1/4))/(1-(p^(1/3))))^0.1+b6*(1/dbh)+b7*(h^(1-(hi/h)^(1/3)))+b8*((1-(hi/h)^(1/4))/(1-(p^(1/3))))) - di)^2
+    (b0*(dbh^b1)*(h^b2)*((1-(hi/h)^(1/4))/(1-(p^(1/3))))^(b3*(hi/h)^4+b4*(1/exp(dbh/h))+b5*((1-(hi/h)^(1/4))/(1-(p^(1/3))))^0.1+b6*(1/dbh)+b7*(h^(1-(hi/h)^(1/3)))+b8*((1-(hi/h)^(1/4))/(1-(p^(1/3))))) - di)^2
   }
 
   stats::optimise(fun_opt_hi,lower=0,upper=h,tol=0.0001)$minimum
